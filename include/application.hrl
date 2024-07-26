8a756147f282201bc312dc65e28a7931bd3f56b7
joq62 <joq62@c50>
2024-07-26 23:06:09 +0200

    created

---
+++ include/application.hrl
@@ -0,0 +1,9 @@
+%% 
+%-define(MainDir,"catalog").
+
+-define(RepoGit,"https://github.com/joq62/catalog_specs.git").
+-define(RepoDir,"catalog_specs").
+-define(ApplicationDir,"application_dir").
+-define(Interval,20*1000).
+
+
